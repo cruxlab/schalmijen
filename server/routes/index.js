63df0b50ce3aa28a9ea24c8f33b3f0587cd6babf
Christian Leuzinger <cleuzinger@luware.com>
2017-06-30 10:58:27 +0200

    add missing files

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express';
+
+
+let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.writeHead('200', {'Content-Type' : 'text/plain'});
+  res.end('Hello World\n');
+  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
